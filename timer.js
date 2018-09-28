4148b67a4811de71867eb11e2e092dba6ce896e3
NPodolyaka <natalia.podolyaka@ukr.net>
2018-09-28 10:44:27 +0300

    Merge branch 'master' of /Users/npodo/WebstormProjects/streams with conflicts.

--- timer.js
+++ timer.js
@@ -1,11 +1,29 @@
-module.exports = function timer(limit, interval) {
-    
-      return setInterval(() => {
-        const usedMemory = Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100;
-            if(usedMemory <= limit){
-                return console.log(usedMemory);
-            }
-            throw new Error(`Memory usage > ${limit} Mb`);
-    },interval);
+module.exports = function timer() {
+    const interval = setInterval(()=>{
+        const memory = Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100;
+                if(memory <= 30) {
+                    return console.log(`The script uses ${memory} Mb`);
+                }
+                    return new Error('Memory usage > 30 Mb');
+
+    },300);
+
+set Timeout(()=>{
+        clearInterval(interval);
+
+    },1000);
+    // return new Promise((resolve, reject) => {
+    //     const interval = setInterval(() => {
+    //         const memory = Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100;
+    //         if(memory <= 30){
+    //             resolve(memory);
+    //         }
+    //         else{
+    //             reject(new Error('Memory usage > 30 Mb'));
+    //         }
+    //     }, 300)
+    // }).then ((memory) => {
+    //     console.log(`The script uses ${memory} Mb`);
+    // })
 };
 
